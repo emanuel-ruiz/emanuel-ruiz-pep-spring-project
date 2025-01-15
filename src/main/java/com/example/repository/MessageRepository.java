d155985ef3e44d83b9379ea46971aeab5ae28b86
emanuel Ruiz <spartaneman@gmail.com>
2025-01-15 00:43:56 +0000

    create message service boilerplate

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,11 +1,16 @@
 package com.example.repository;
 
+import java.util.List;
+
 import com.example.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer>{
-
-
+    
+    @Query("SELECT m FROM Message m WHERE m.postedBy = :postedBy")
+    List<Message> findByPostedBy(@Param("postedBy") Integer postedBy);
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -3,9 +3,13 @@ package com.example.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.entity.*;
+import com.example.exception.ResourceNotFoundException;
 import com.example.repository.AccountRepository;
 import com.example.repository.MessageRepository;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MessageService {
@@ -33,5 +37,32 @@ public class MessageService {
     public List<Message> getAllMessages(){
         List<Message> messages = (List) messageRepository.findAll();
         return messages;
+    }
+
+    public Message getMessageById(Integer messageId) throws RuntimeException{
+        Optional<Message> message = messageRepository.findById(messageId);
+        if(!message.isPresent()){
+            throw new ResourceNotFoundException("Message with message_id: " + messageId + " Not Found");
+        }
+        return message.get();
+    }
+
+    public Message updateMessageById(Integer messageId, String messageText) throws RuntimeException{
+        if(messageText.isBlank() || messageText.length() > 255){
+            throw new IllegalArgumentException("Message Text cannot be blank or longer than 255 characters");
+        }
+        Optional<Message> message = messageRepository.findById(messageId);
+        if(!message.isPresent()){
+            throw new ResourceNotFoundException("Message with message_id " + messageId + " Not Found");
+        }
+        Message newMessage = message.get();
+        newMessage.setMessageText(messageText);
+        return messageRepository.save(newMessage);
+    }
+
+    public List<Message> getMessagesByUser(int user_id){
+        List<Message> user_messages = new ArrayList<>();
+        user_messages = messageRepository.findByPostedBy(user_id);
+        return user_messages;
     }
 }
