6a3fb555c6cc6682018877a797927508d3a34d03
emanuel Ruiz <spartaneman@gmail.com>
2025-01-15 00:03:43 +0000

    add message methods

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,10 +1,11 @@
 package com.example.repository;
 
+import com.example.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MessageRepository extends JpaRepository{
+public interface MessageRepository extends JpaRepository<Message, Integer>{
 
 
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,7 +1,37 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.example.entity.*;
+import com.example.repository.AccountRepository;
+import com.example.repository.MessageRepository;
+import java.util.List;
 
 @Service
 public class MessageService {
+
+    private MessageRepository messageRepository;
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository, AccountRepository accountRepository){
+        this.messageRepository = messageRepository;
+        this.accountRepository = accountRepository;
+    }
+
+    public Message createMessage(Message message) throws RuntimeException {
+        if(message.getMessageText().isBlank() || message.getMessageText().length() > 255 ){
+            throw new IllegalArgumentException("Message cannot be blank or be larger than 255 characters");
+        }
+        if(!accountRepository.existsById(message.getPostedBy())){
+            throw new IllegalArgumentException("Posted by user is not registered");
+        }
+
+        return messageRepository.save(message);
+    }
+
+    public List<Message> getAllMessages(){
+        List<Message> messages = (List) messageRepository.findAll();
+        return messages;
+    }
 }
